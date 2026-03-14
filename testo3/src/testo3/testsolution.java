4ee71368eed9229b0fe26f4f4dd32f2cdc1f36f0
pranaybhoware <pranaybhoware2701@gmail.com>
2026-03-14 13:39:01 +0530

    done solution

---
+++ testo3/src/testo3/testsolution.java
@@ -0,0 +1,36 @@
+package testo3;
+
+import java.util.Scanner;
+
+public class testsolution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            int num;
+
+            if (i % 2 == 0) {
+                num = 0;
+            } else {
+                num = 1;
+            }
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(num + " ");
+                num = (num == 0) ? 1 : 0;
+
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
